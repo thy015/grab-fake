7ac3c04858ff328c4c82e0f233c78b96c1722dd5
thy <thymai.1510@gmail.com>
2024-04-08 01:11:13 +0700

    intialiazed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ListGroup from "./components/ListGroup"
+function App(){
+  return (
+    <><h1>Đây là 1 cái list</h1>
+    <ul class="list-group">
+      <li class="list-group-item">An active item</li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+      <li class="list-group-item">A fourth item</li>
+      <li class="list-group-item">And a fifth one</li>
+    </ul></>
+  ) //spread to mutiple lines
+}
+export default App
---
+++ src/components/ListGroup.jsx
@@ -0,0 +1,4 @@
+function ListGroup(){
+    return <h1>List Group</h1>
+}
+export default ListGroup

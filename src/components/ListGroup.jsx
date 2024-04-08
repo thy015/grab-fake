670b92c668424296ff3150c6e4f9bbe58317438d
thy <thymai.1510@gmail.com>
2024-04-08 15:59:41 +0700

    message

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,4 +1,17 @@
-function ListGroup(){
-    return <h1>List Group</h1>
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Sai Gon", "Ha Noi", "Tokyo"];
+//   const message = () =>{items.length===0 ? <p>No items</p> : null}
+  const getMessage=()=>{items.length===0 && <p>No items</p>}
+  return (
+    <>
+      <h1>Đây là 1 cái list</h1>
+      {getMessage()}
+      <ul class="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  ); //spread to mutiple lines
 }
-export default ListGroup
+export default ListGroup;

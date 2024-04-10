b767f58499a032eb810c5b3796e8086fc7c76474
thy <thymai.1510@gmail.com>
2024-04-11 02:35:18 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import FluidButton from "./components/FluidButton"
 import FoodCategory from "./components/FoodCategory"
 import BackGroundImg from "./components/BackGroundImg"
 import WhyShouldOrder from "./components/WhyShouldOrder"
-import WhatIsGrabFood from "./components/WhatIsGrabFood"
+import WhatsGrabFood from "./components/WhatsGrabFood"
 function App(){
   return <><div><Header></Header>
   <BackGroundImg></BackGroundImg>
@@ -22,7 +22,8 @@ function App(){
   <Words words="Vì sao bạn nên Order trên GrabFood?"></Words>
   <WhyShouldOrder ></WhyShouldOrder>
   <Words words="Những câu hỏi thường gặp"></Words>
-
+  <WhatsGrabFood title="GrabFood là gì?" describe="Lunch, Bún Cá Chấm Gốc Đa - Vũ Thạnh for Dinner! We are here to satisfy your hunger with a wide selection of merchant partners in Vietnam. GrabFood là dịch vụ Giao đồ ăn nhanh nhất tại Việt Nam. Chúng tôi đã sắp xếp tất cả các món ăn, nhà hàng và thực phẩm yêu thích của bạn một cách hợp lý để giúp bạn tìm được đồ ăn dễ dàng và nhanh chóng nhất có thể. Tìm và đặt món ăn yêu thích trên khắp Việt Nam - đặt đồ ăn trực tuyến chỉ bằng vài thao tác, từ món Lifted Coffee & Brunch cho bữa sáng, đến Maazi Indian – Nhà Hàng Ấn Độ cho bữa trưa, đến Bún Cá Chấm Gốc Đa – Vũ Thạnh cho bữa tối! Hãy để chúng tôi xua tan cơn đói của bạn nhờ một loạt đối tác bán đồ ăn ở Việt Nam."></WhatsGrabFood>
+  <FluidButton buttonText="Read More"/>
   </>
 }
 export default App
--- src/components/WhyShouldOrder.jsx
+++ src/components/WhyShouldOrder.jsx
@@ -7,8 +7,6 @@ function WhyShouldOrder() {
           <li>
             <div className="spacing-order"><div>
             </div>
-              <img src="./src/assets/check.png" className="check" />
-              <span className="order-space"></span>
               <span className="fw-bold why-words">Nhanh nhất </span>
               <span className="why-words">
                 - GrabFood cung cấp dịch vụ giao đồ ăn nhanh nhất thị trường.
@@ -17,8 +15,6 @@ function WhyShouldOrder() {
           </li>
           <li>
             <div className="spacing-order">
-              <img src="./src/assets/check.png" className="check" />
-              <span className="order-space"></span>
               <span className="fw-bold why-words">Dễ dàng nhất </span>
               <span className="why-words">
                 - Giờ đây, bạn chỉ cần thực hiện vài cú nhấp chuột hoặc chạm nhẹ
@@ -30,8 +26,6 @@ function WhyShouldOrder() {
           </li>
           <li>
             <div className="spacing-order">
-              <img src="./src/assets/check.png" className="check" />
-              <span className="order-space"></span>
               <span className="fw-bold why-words">Đáp ứng mọi nhu cầu </span>
               <span className="why-words">
                 - Từ món ăn đặc sản địa phương đến các nhà hàng được ưa thích,
@@ -41,9 +35,7 @@ function WhyShouldOrder() {
             </div>
           </li>
           <li>
-            <div className="spacing-order">
-              <img src="./src/assets/check.png" className="check" />
-              <span className="order-space"></span>
+            <div className="spacing-order">         
               <span className="fw-bold why-words">Thanh toán dễ dàng </span>
               <span className="why-words">
                 - Giao và nhận đồ ăn thật dễ dàng. Thanh toán bằng GrabPay thậm
@@ -53,8 +45,6 @@ function WhyShouldOrder() {
           </li>
           <li>
             <div className="spacing-order">
-              <img src="./src/assets/check.png" className="check" />
-              <span className="order-space"></span>
               <span className="fw-bold why-words">Nhiều quà tặng hơn </span>
               <span className="why-words">
                 - Tích điểm GrabRewards cho mỗi đơn hàng của bạn và sử dụng điểm

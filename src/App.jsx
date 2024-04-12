ebfdddc528ed204464111a38d17dc45c24a47119
thy <thymai.1510@gmail.com>
2024-04-13 02:02:15 +0700

    optimize code

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
 import Header from "./components/Header";
-import Words from "./components/Words";
-import SeachBox from "./components/SearchBox";
-import CardHolder from "./components/CardHolder";
-import FluidButton from "./components/FluidButton";
-import FoodCategory from "./components/FoodCategory";
-import BackGroundImg from "./components/BackGroundImg";
-import WhyShouldOrder from "./components/WhyShouldOrder";
-import WhatsGrabFood from "./components/WhatsGrabFood";
-import NavButton from "./components/NavButton";
+import Body_Home from "./components/Body_Home";
 import Footer from "./components/Footer";
 import { useState } from "react";
+import Detail from "./components/Detail";
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
   //read more button
-  const[show, setShow]=useState(false)
+  const [show, setShow] = useState(false);
 
-  
-  return (  
+  return (
     <>
       <div>
         <Header></Header>
-        <BackGroundImg></BackGroundImg>
-        <Words words="Ưu đãi GrabFood tại " inputplace="InputPlace"></Words>
-        <SeachBox></SeachBox>
+        <Body_Home></Body_Home>
       </div>
-      <div>
-        <CardHolder ></CardHolder>
-      </div>
-      <NavButton buttonText="See All Promotions" />
-      <Words words="There's something for everyone!"></Words>
-      <FoodCategory></FoodCategory>
-      <Words words="Vì sao bạn nên Order trên GrabFood?"></Words>
-      <WhyShouldOrder></WhyShouldOrder>
-      <Words words="Những câu hỏi thường gặp"></Words>
-      <WhatsGrabFood
-        title="GrabFood là gì?"
-        describe="Lunch, Bún Cá Chấm Gốc Đa - Vũ Thạnh for Dinner! We are here to satisfy your hunger with a wide selection of merchant partners in Vietnam. GrabFood là dịch vụ Giao đồ ăn nhanh nhất tại Việt Nam. Chúng tôi đã sắp xếp tất cả các món ăn, nhà hàng và thực phẩm yêu thích của bạn một cách hợp lý để giúp bạn tìm được đồ ăn dễ dàng và nhanh chóng nhất có thể. Tìm và đặt món ăn yêu thích trên khắp Việt Nam - đặt đồ ăn trực tuyến chỉ bằng vài thao tác, từ món Lifted Coffee & Brunch cho bữa sáng, đến Maazi Indian – Nhà Hàng Ấn Độ cho bữa trưa, đến Bún Cá Chấm Gốc Đa – Vũ Thạnh cho bữa tối! Hãy để chúng tôi xua tan cơn đói của bạn nhờ một loạt đối tác bán đồ ăn ở Việt Nam."
-      ></WhatsGrabFood>
-      <FluidButton buttonText="Read More">
-      </FluidButton>
       <Footer></Footer>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import { BrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
